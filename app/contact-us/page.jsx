103ce28c7018bb1e038da20bb6a5e556d889e7bd
HARSHIKANT <harshikantdubey999@gmail.com>
2025-10-10 10:08:03 +0530

    Abous-us, contact pages added

---
+++ app/contact-us/page.jsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+function Contact() {
+  return (
+    <>
+    <Navbar />
+    <div className="flex flex-col items-start px-6 md:px-16 lg:px-32 bg-orange-50">
+        <div className="flex flex-col items-end pt-12">
+            <p className="text-2xl font-medium">Contacts</p>
+            <div className="w-16 h-0.5 bg-orange-600 rounded-full"></div>
+        </div>
+    </div>
+    <Footer />
+</>
+  )
+}
+
+export default Contact
